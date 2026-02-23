49bfda9b37de1870743ed7c7a6e0d09efe1eae67
hupeyaszih <hupeya@protonmail.com>
2026-02-23 18:46:33 +0300

    fix(nn_memory): clear_time_window function clear events

--- src/nn_memory.c
+++ src/nn_memory.c
@@ -10,7 +10,6 @@
 struct EventList{
    unsigned int* time;
    int* neuron_id;
-   bool* done;
 
    size_t capacity; 
    size_t size;
@@ -19,6 +18,7 @@ struct EventList{
 struct TimeTable{
     int size;
     TimeWindow* time_windows;
+    EventList* event_list;
 };
 
 struct TimeWindow{
@@ -33,6 +33,11 @@ struct TimeWindow{
 //Now can overwrite events onto time_table, we don't need to free them because we still need them (while simulation running)
 int clear_time_window(TimeTable* time_table,int time_window_id, int new_target_time){
     time_table->time_windows[time_window_id].target_time = new_target_time;
+    for(int i = 0;i < time_table->time_windows[time_window_id].event_count;i++){
+        int id = i + time_table->time_windows[time_window_id].event_start_idx;
+        time_table->event_list->time[id] = -1;
+        time_table->event_list->neuron_id[id] = -1;
+    }
     return NN_SUCCESS;
 }
 
@@ -53,6 +58,7 @@ TimeTable* create_time_table(int capacity){
         time_table->time_windows[i].target_time = -1;
         time_table->time_windows[i].event_start_idx = -1;
         time_table->time_windows[i].event_count = 0;
+        time_table->time_windows[i].done = false;
     }
     return time_table;
 }
@@ -72,21 +78,18 @@ int add_event(EventList* event_list, int target_time, int neuron_id) {
     int id = event_list->size + 1;
     event_list->time[id] = target_time;
     event_list->neuron_id[id] = neuron_id;
-    event_list->done[id] = false;
     return NN_SUCCESS;
 }
 int delete_event(EventList* event_list,int event_id) {
     if(event_id >= event_list->size) return NN_FAILED_TO_DELETE_EVENT;
     event_list->time[event_id] = -1;
     event_list->neuron_id[event_id] = -1;
-    event_list->done[event_id] = false;
     return NN_SUCCESS;
 }
 
 int delete_event_list(EventList* event_list) {
     if(event_list->time){free(event_list->time); event_list->time = NULL;}
     if(event_list->neuron_id){free(event_list->neuron_id); event_list->neuron_id = NULL;}
-    if(event_list->done){free(event_list->done); event_list->done = NULL;}
     if(event_list){free(event_list); event_list = NULL;}
     return NN_SUCCESS;
 }
@@ -94,6 +97,5 @@ EventList* create_event_list(int capacity) {
     EventList* event_list = malloc(sizeof(EventList));
     event_list->time = malloc(sizeof(unsigned int) * capacity);
     event_list->neuron_id = malloc(sizeof(int) * capacity);
-    event_list->done = malloc(sizeof(bool) * capacity);
     return event_list;
 }
